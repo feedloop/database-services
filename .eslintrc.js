de17149c6e7a862a6fdd069b87b6f1e00ce63067
Rifa Sania <rifasania@upi.edu>
2025-02-11 15:18:30 +0700

    implement linter and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    env: {
+      browser: false,
+      node: true,
+      es6: true,
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "prettier"
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "script",
+    },
+    plugins: ["@typescript-eslint", "prettier"],
+    rules: {
+      "prettier/prettier": "error",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  };
+  
