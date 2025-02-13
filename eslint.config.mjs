a3d5f3e652e255eb866a33b7ff311fa5cb04b976
Rifa Sania <rifasania@upi.edu>
2025-02-13 12:16:26 +0700

    fixed all

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,32 +1,3 @@
-// import js from "@eslint/js";
-// import tseslint from "@typescript-eslint/eslint-plugin";
-// import tsparser from "@typescript-eslint/parser";
-
-// export default [
-//   js.configs.recommended,
-//   {
-//     languageOptions: {
-//       parser: tsparser,
-//     },
-//     plugins: {
-//       "@typescript-eslint": tseslint,
-//     },
-//     rules: {
-//       "@typescript-eslint/no-var-requires": "off",
-//     },
-//   },
-//   // {
-//   //   files: ["src/migrations/*.js", "src/config/sequelize.config.js"],
-//   //   languageOptions: {
-//   //     sourceType: "commonjs",
-//   //   },
-//   //   rules: {
-//   //     "no-undef": "off",
-//   //     "@typescript-eslint/no-var-requires": "off",
-//   //   },
-//   // },
-// ];
-
 import js from "@eslint/js";
 import tseslint from "@typescript-eslint/eslint-plugin";
 import tsparser from "@typescript-eslint/parser";
